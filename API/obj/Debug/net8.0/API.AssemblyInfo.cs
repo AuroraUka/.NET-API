e066848cd3b075a1bca690fa52e4ab06940ac2dd
AuroraUka <au57198@ubt-uni.net>
2024-03-14 07:45:35 +0100

    Last commit

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c18b482c6b62f5280cbfc1e8a07b306864426c65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57f5329f3a8bfd1ef1a458838c0622b07ed25fdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
